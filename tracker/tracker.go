837b78787983f7cad1e0b128fa75fc0af68ee740
Matthias Harzer <matthias.harzer03@gmail.com>
2026-08-20 15:00:45 +0200

    fix: log error when extracting player stats

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -36,6 +36,7 @@ func (s *StatsTracker) Submit(userID string, imageData []byte, date time.Time) e
 
 	stats, err := s.analyzer.ExtractPlayerStats(imageData)
 	if err != nil {
+		logging.Error("failed to extract player stats", "err", err)
 		return fmt.Errorf("failed to extract player XP: %w", err)
 	}
 
