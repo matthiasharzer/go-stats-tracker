8d4b47ca376fbc0dc7e1a742f20d7dce0ba17421
Matthias Harzer <matthias.harzer03@gmail.com>
2026-08-20 17:20:44 +0200

    fix: make logging more verbose

--- api/v1/ingest/handler.go
+++ api/v1/ingest/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/docker/go-units"
+	"github.com/matthiasharzer/go-stats-tracker/logging"
 	"github.com/matthiasharzer/go-stats-tracker/tracker"
 	"github.com/matthiasharzer/go-stats-tracker/utils/timeutils"
 )
@@ -14,17 +15,20 @@ const fileSizeLimit = 10 * units.MiB
 func Handler(tracker *tracker.StatsTracker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
+			logging.Warn("method not allowed")
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
 		userID := r.PathValue("userID")
 		if userID == "" {
+			logging.Warn("missing userID in request path")
 			http.Error(w, "userID is required", http.StatusBadRequest)
 			return
 		}
 
 		if r.Body == nil {
+			logging.Warn("missing request body in request", "userID", userID)
 			http.Error(w, "missing request body", http.StatusBadRequest)
 			return
 		}
@@ -35,6 +39,7 @@ func Handler(tracker *tracker.StatsTracker) http.HandlerFunc {
 		limitedReader := http.MaxBytesReader(w, r.Body, int64(fileSizeLimit))
 		fileContent, err := io.ReadAll(limitedReader)
 		if err != nil {
+			logging.Warn("failed to read request body", "error", err, "userID", userID)
 			http.Error(w, "failed to read request body", http.StatusBadRequest)
 			return
 		}
@@ -43,6 +48,7 @@ func Handler(tracker *tracker.StatsTracker) http.HandlerFunc {
 
 		err = tracker.Submit(userID, fileContent, today)
 		if err != nil {
+			logging.Warn("failed to submit stats", "error", err, "userID", userID)
 			http.Error(w, "failed to submit stats", http.StatusBadRequest)
 			return
 		}
