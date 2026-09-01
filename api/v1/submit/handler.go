e0288b3adc59451d7ace56c9d20563241008b903
Matthias Harzer <matthias.harzer03@gmail.com>
2026-09-01 13:26:31 +0000

    feat: rename /ingest to /submit

--- api/v1/submit/handler.go
+++ api/v1/submit/handler.go
@@ -1,4 +1,4 @@
-package ingest
+package submit
 
 import (
 	"errors"
--- cmd/run/get_mux.go
+++ cmd/run/get_mux.go
@@ -7,7 +7,7 @@ import (
 	"github.com/matthiasharzer/go-stats-tracker/api/v1/auth/callback"
 	"github.com/matthiasharzer/go-stats-tracker/api/v1/auth/link"
 	"github.com/matthiasharzer/go-stats-tracker/api/v1/auth/register"
-	"github.com/matthiasharzer/go-stats-tracker/api/v1/ingest"
+	"github.com/matthiasharzer/go-stats-tracker/api/v1/submit"
 	"github.com/matthiasharzer/go-stats-tracker/persistence"
 	"github.com/matthiasharzer/go-stats-tracker/tracker"
 	"golang.org/x/oauth2"
@@ -25,7 +25,7 @@ func getMux(oauth oauth2.Config, database persistence.Database, tracker *tracker
 	mux.HandleFunc("GET /api/v1/auth/register", register.Handler(sharedState, oauth))
 	mux.HandleFunc("GET /api/v1/auth/callback", callback.Handler(sharedState, oauth, filePickerAPIKey, filePickerAppID))
 	mux.HandleFunc("POST /api/v1/auth/link", link.Handler(sharedState, database))
-	mux.HandleFunc("POST /api/v1/ingest/{sheetID}", ingest.Handler(database, tracker))
+	mux.HandleFunc("POST /api/v1/submit/{sheetID}", submit.Handler(database, tracker))
 
 	return mux
 }
